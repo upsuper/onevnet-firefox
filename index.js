ab64b6467277841c4edca47b7c428d40f30408df
Xidorn Quan <me@upsuper.org>
2016-07-24 15:24:45 +1000

    Update data from official extension 2.5

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ var button = ToggleButton({
 var panel = Panel({
   contentURL: "./panel.html",
   contentScriptFile: "./panel.js",
-  width: 200,
-  height: 580,
+  width: 380,
+  height: 375,
   onHide: () => {
     button.state('window', {checked: false});
   }
@@ -40,10 +40,14 @@ let pac = {
   us: 'http://pac.vnet.one/11',
   sg: 'http://pac.vnet.one/12',
   fr: 'http://pac.vnet.one/10',
+  hk: 'http://pac.vnet.one/286',
+  in: 'http://pac.vnet.one/734',
   jp_all: 'http://pac.vnet.one/13',
   us_all: 'http://pac.vnet.one/15',
   sg_all: 'http://pac.vnet.one/16',
   fr_all: 'http://pac.vnet.one/14',
+  hk_all: 'http://pac.vnet.one/287',
+  in_all: 'http://pac.vnet.one/735',
 };
 
 function switchProxy(name = "") {
