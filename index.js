57bde3f3494165eea0da564e47034e2264e8c430
Xidorn Quan <me@upsuper.org>
2017-03-19 23:30:32 +1100

    Upgrade to 2.9.6 of Chrome version

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var panel = Panel({
   contentURL: "./panel.html",
   contentScriptFile: "./panel.js",
   width: 200,
-  height: 410,
+  height: 485,
   onHide: () => {
     button.state('window', {checked: false});
   }
